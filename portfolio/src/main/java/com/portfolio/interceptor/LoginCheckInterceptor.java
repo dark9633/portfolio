2c942f96584583eb93dd355d4f72dda655423c87
pc <pc@pc-pc>
2018-10-20 18:33:59 +0900

    로그인 검증 인터셉터 추가 | 게시글작성 적용

---
+++ portfolio/src/main/java/com/portfolio/interceptor/LoginCheckInterceptor.java
@@ -0,0 +1,24 @@
+package com.portfolio.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LoginCheckInterceptor extends HandlerInterceptorAdapter {
+
+	//private static final Logger logger = LoggerFactory.getLogger(LoginCheckInterceptor.class);
+			
+	@Override
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws Exception {
+
+		HttpSession session = request.getSession();
+		if(session.getAttribute("member") == null){
+			response.sendRedirect("/");
+		}
+		return super.preHandle(request, response, handler);
+	}
+
+}
